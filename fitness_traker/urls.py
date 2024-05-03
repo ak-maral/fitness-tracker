bf7870aa05c3c6dd2d2d61d89ce3f530ef76261a
ak-maral <akmaral.nurbekkyzy1@gmail.com>
2024-05-03 13:15:06 +0600

    created workout and activities apps

--- fitness_traker/urls.py
+++ fitness_traker/urls.py
@@ -19,5 +19,17 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("users.urls")),
+    path('users/', include("users.urls")),
 ]
+
+# urlpatterns = [
+#     path("users/", include("users.urls")),
+#     path('admin/', admin.site.urls),
+#     # path('soft/', soft),
+# ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -5,6 +5,8 @@ class User(models.Model):
     username = models.CharField(max_length=10)
     email = models.EmailField()
     phonenumber = models.CharField(max_length=10)
+    age = models.IntegerField(null=True, blank=True)  
+    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True) 
 
     def __str__(self):
         return self.email
