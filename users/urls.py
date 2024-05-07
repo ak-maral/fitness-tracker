2345222219507b13ca6e3ad7ca4452f9ac36d04f
ak-maral <akmaral.nurbekkyzy1@gmail.com>
2024-05-07 23:18:39 +0600

    added serializers

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
+from rest_framework import routers
 from django.urls import path
-from users import views
+from .views import UserModelViewSet
 
-urlpatterns = [
-    path('', views.Home, name="Home"),
-    path('signup', views.Signup, name="Signup"),
-    path('login', views.Login, name="Login"),
-    path('logout', views.Logout, name='Logout')
+router = routers.DefaultRouter()
+
+router.register(
+    "users", viewset=UserModelViewSet, basename="users"
+)
+
+urlpatterns= [
+
 ]
+
+urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,70 +1,81 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+# from django.shortcuts import render, redirect
+# from django.contrib import messages
+# from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
-def Home(request):
-    return render(request, 'index.html')
+# # Create your views here.
+# def Home(request):
+#     return render(request, 'index.html')
 
-def Signup(request):
-    if request.method=="POST":
-        username = request.POST.get('username')
-        phonenumber=request.POST.get('phonenumber')
-        email=request.POST.get('email')
-        pass1=request.POST.get('pass1')
-        pass2=request.POST.get('pass2')
+# def Signup(request):
+#     if request.method=="POST":
+#         username = request.POST.get('username')
+#         phonenumber=request.POST.get('phonenumber')
+#         email=request.POST.get('email')
+#         pass1=request.POST.get('pass1')
+#         pass2=request.POST.get('pass2')
       
-        if len(phonenumber)>10 or len(phonenumber)<10:
-            messages.info(request,"Phone Number Must be 10 Digits")
-            return redirect('/signup')
+#         if len(phonenumber)>10 or len(phonenumber)<10:
+#             messages.info(request,"Phone Number Must be 10 Digits")
+#             return redirect('/signup')
 
-        if pass1!=pass2:
-            messages.info(request,"Password is not Matching")
-            return redirect('/signup')
+#         if pass1!=pass2:
+#             messages.info(request,"Password is not Matching")
+#             return redirect('/signup')
        
-        try:
-            if User.objects.get(phonenumber=phonenumber):
-                messages.warning(request,"Phone Number is already taken")
-                return redirect('/signup')
+#         try:
+#             if User.objects.get(phonenumber=phonenumber):
+#                 messages.warning(request,"Phone Number is already taken")
+#                 return redirect('/signup')
            
-        except Exception as identifier:
-            pass
+#         except Exception as identifier:
+#             pass
         
         
-        try:
-            if User.objects.get(email=email):
-                messages.warning(request,"Email is already taken")
-                return redirect('/signup')
+#         try:
+#             if User.objects.get(email=email):
+#                 messages.warning(request,"Email is already taken")
+#                 return redirect('/signup')
         
-        except Exception as identifier:
-            pass
-        
-        
-        
-        myuser=User.objects.create_user(username,phonenumber,email,pass1)
-        myuser.save()
-        messages.success(request,"User is Created Please Login")
-        return redirect('/login')
+#         except Exception as identifier:
+#             pass
         
         
-    return render(request,"signup.html")
+        
+#         myuser=User.objects.create_user(username,phonenumber,email,pass1)
+#         myuser.save()
+#         messages.success(request,"User is Created Please Login")
+#         return redirect('/login')
+        
+        
+#     return render(request,"signup.html")
 
-def Login(request):
-    if request.method=="POST":        
-        username=request.POST.get('username')
-        pass1=request.POST.get('pass1')
-        myuser=authenticate(username=username,password=pass1)
-        if myuser is not None:
-            login(request,myuser)
-            messages.success(request,"Login Successful")
-            return redirect('/')
-        else:
-            messages.error(request,"Invalid Credentials")
-            return redirect('/login')
-    return render(request, 'login.html')
+# def Login(request):
+#     if request.method=="POST":        
+#         username=request.POST.get('username')
+#         pass1=request.POST.get('pass1')
+#         myuser=authenticate(username=username,password=pass1)
+#         if myuser is not None:
+#             login(request,myuser)
+#             messages.success(request,"Login Successful")
+#             return redirect('/')
+#         else:
+#             messages.error(request,"Invalid Credentials")
+#             return redirect('/login')
+#     return render(request, 'login.html')
 
-def Logout(request):
-    logout(request)
-    messages.success(request,"Logout Success")    
-    return redirect('/login')
+# def Logout(request):
+#     logout(request)
+#     messages.success(request,"Logout Success")    
+#     return redirect('/login')
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
+from rest_framework import viewsets
+from .serializers import UserSerializers
+
+
+class UserModelViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
