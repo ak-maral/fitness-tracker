1ee31bb7493d19e4cf604f6f30fde4af9e919b69
ak-maral <akmaral.nurbekkyzy1@gmail.com>
2024-05-09 21:30:36 +0600

    added some specifications to trainer model and workout model

--- trainers/models.py
+++ trainers/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 class Trainer(models.Model):
     username = models.CharField(max_length=10)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     phonenumber = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(blank=True) 
+    profile_picture = models.ImageField(upload_to='trainers/profile_pictures/', blank=True)
+    certification = models.CharField(max_length=50, blank=True) 
+    specialization = models.CharField(max_length=50, blank=True) 
+    experience = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.username
--- workout/models.py
+++ workout/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from trainers.models import Trainer
 
 class Workout(models.Model):
@@ -17,6 +18,8 @@ class Workout(models.Model):
     image = models.ImageField(upload_to='images/', null=True, blank=True)
     duration = models.FloatField(default=0)  # Duration of the video in seconds
     trainer = models.ForeignKey(to = Trainer, on_delete=models.SET_NULL, null=True, related_name="trainer")
+    created_at = models.DateTimeField(default=timezone.now()),
+    updated_at = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
         return self.name
