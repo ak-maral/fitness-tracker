af875bf5462400f9569d0c5075fe5a0a7585b4e3
ak-maral <akmaral.nurbekkyzy1@gmail.com>
2024-05-17 10:57:51 +0600

    changes

--- workout/serializers.py
+++ workout/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Workout
-
+class WorkoutSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = '__all__'
 
 class WorkoutCreateSerializers(serializers.ModelSerializer):
     class Meta:
--- workout/urls.py
+++ workout/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import path,include
 from . import views
 
+router = routers.DefaultRouter()
+# router.register('workout', views.WorkoutViewSet, basename='Workout')
+
 urlpatterns = [
-    path("workout/", views.WorkoutViewSet.as_view({'get': 'list', 'post': 'create', 'put': 'update'}), name="workout"),
-    path("workout/<int:pk>/", views.WorkoutViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name="workout")
+    # path("workout/", WorkoutModelViewSet.as_view({'get': 'list', 'post': 'create', 'put': 'update'}), name="workout"),
+    path('workout/', views.WorkoutListViewSet.as_view()),
+    path('workout/', views.WorkoutCreateViewSet.as_view()),
+    path('workout/<int:pk>/', views.WorkoutDetailViewSet.as_view()),
 ]
